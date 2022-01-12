6154c0809a524ec24b55a9e04b9de46b694ea2d0
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-12 14:14:15 -0800

    letterPositiions.js

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+    
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(eqArrays(actual, expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // go through every letter of the sentence
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]]) {
+      results[sentence[i]] = [];
+    }
+    results[sentence[i]].push(i);
+    // if it exists in the object, push index of sentence[i]
+    
+    // if it doesn't exist, create array and push index in there
+  }
+  console.log("result:", results);
+
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").h,[0]);
+assertArraysEqual(letterPositions("hello").e,[1]);
+assertArraysEqual(letterPositions("hello").l,[2,3]);
