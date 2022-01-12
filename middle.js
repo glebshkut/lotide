6c821132bd65dcba6eb452192fe97d366b39ad6f
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-12 08:33:58 -0800

    middle.js

---
+++ middle.js
@@ -0,0 +1,38 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i] || arr1.length !== arr2.length) {
+      return false;
+    }
+    
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  console.log(eqArrays(actual, expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+};
+
+const middle = function(arr) {
+  if (arr.length <= 2) {
+    return [];
+  } 
+  if (arr.length % 2 === 1) {
+    return [arr[Math.floor(arr.length/2)]];
+  }
+  if(arr.length % 2 === 0) {
+    return arr.slice(arr.length/2 - 1, arr.length/2 + 1);
+  }
+}
+
+
+
+
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+assertArraysEqual(middle([1, 2, 3]),[2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1, 2]),[]);
+assertArraysEqual(middle([]),[]);
