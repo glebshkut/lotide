d187c1a2ec6b630e7fc0b20e23b061d0954b6373
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-11 19:45:56 -0800

    added flatten.js

--- without.js
+++ without.js
@@ -29,7 +29,7 @@ const without = function(source, itemsToRemove) {
 
 
 console.log("total",without([1, 2, 3], [1])); // => [2, 3]
-console.log("total",without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log("total",without([1, 2, "1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 console.log("total",without(["1", "2", "3",4], [1, 2, "3",5-1]));
 
 const words = ["hello", "world", "lighthouse"];
