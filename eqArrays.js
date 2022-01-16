f3f83053bb6bd79969a9432ea856205ef09b4237
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-16 10:52:10 -0800

    eqArrays.js finishing

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,11 @@ const eqArrays = function(arr1, arr2) {
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else 
     if (arr1[i] !== arr2[i]) {
       return false;
     }
@@ -15,9 +20,14 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3, [2, [12, 0]]], [4]], [[2, 3, [2, [12, 0]]], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
