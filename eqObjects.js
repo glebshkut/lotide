5d7e99e44f3d49370d529b365150023460108930
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-15 20:51:22 -0800

    eqObjects.js works with nested objects now

--- eqObjects.js
+++ eqObjects.js
@@ -28,9 +28,18 @@ const eqObjects = function(object1, object2) {
       return false;
     }
   }
-  const keys = Object.keys(object1);
+
+
+  const keys = Object.keys(object1); // keys = object 1 keys
   for (let i = 0; i < keys.length; i++) {
-    if (Array.isArray(object1[keys[i]]) && Array.isArray(object2[keys[i]])) { // check if array
+    let bool = Array.isArray(object1[keys[i]]) && Array.isArray(object2[keys[i]]);
+  if (!bool && typeof object1[keys[i]] == "object" && typeof object2[keys[i]] == "object") {
+    if (!eqObjects(object1[keys[i]], object2[keys[i]])) {
+      return false;
+    }
+    continue;
+  
+  } else if (bool) { // check if array
       if (!(eqArrays(object1[keys[i]],object2[keys[i]]))) {
         return false;
       }
@@ -59,3 +68,8 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
