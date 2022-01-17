ed611fc22a0bbb6cfeda70279789c9fa8610c601
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-17 08:34:42 -0800

    added tests to assertEqual and head.js

--- head.js
+++ head.js
@@ -1,12 +1,7 @@
-const assertEqual = function(actual, expected) {
-  console.log(actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require("./assertEqual");
 
 const head = function(arr) {
   return arr[0];
 }
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 1.1);
+assertEqual("Boo", "Boo");
+assertEqual("Boo", "BOo");
