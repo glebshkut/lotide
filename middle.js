a66e20447e555c4b376e0ef74d13431b36b156c0
Gleb Shkut <gleb.shkut@gmail.com>
2022-01-17 09:05:08 -0800

    mocha&chai for headTest.js

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-    
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function(actual, expected) {
-  console.log(eqArrays(actual, expected) ? `✅✅✅ Assertion Passed: ${actual} === ${expected}` : `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-};
 
 const middle = function(arr) {
   if (arr.length <= 2) {
@@ -32,10 +14,4 @@ const middle = function(arr) {
 
 
 
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2]),[]);
-assertArraysEqual(middle([]),[]);
+module.exports = middle;
